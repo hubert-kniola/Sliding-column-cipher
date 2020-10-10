867221e1e906282e121200caaa04455403834bf2
Hubert <hubert.kniola@student.put.poznan.pl>
2020-10-10 20:43:14 +0200

    v1

---
+++ ClassLibrary1/Encryption.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Encryption
+    {
+        static public void Encrypt(string keyWord, string plainText)
+        {
+            int k = 0;
+            int n = keyWord.Length;
+            int m = plainText.Length / n + 1;
+            Console.WriteLine("n: " + n + "| m: " + m);
+            char[] charTable = plainText.ToCharArray();
+            char[,] normalTable = new char[n, m];
+            char[,] encryptTable = new char[n + m - 1, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (k >= charTable.Length)
+                        normalTable[i, j] = 'x';
+                    else
+                        normalTable[i, j] = charTable[k];
+                    k++;
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (normalTable[i, j] == ' ')
+                        Console.Write(" _ ");
+                    else
+                        Console.Write($" {normalTable[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    encryptTable[i, j] = normalTable[i, j];
+                }
+            }
+        }
+    }
+}
---
+++ POD Szyfr PK/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ClassLibrary1;
+
+namespace POD_Szyfr_PK
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            int x = default;
+            string plainText = default, keyWord = default;
+            Encryption enc = new Encryption();
+            try
+            {
+                do
+                {
+                    Console.WriteLine("SZYFR PRZEKATNOKOLUMNOWY by Hubert Knioła");
+                    Console.Write("0. Zakoncz dzialanie programu\n1. Szyfrowanie\n2. Deszyfrowanie\n");
+                    Console.Write("Podaj odpowiedni numer: ");
+                    x = Int32.Parse(Console.ReadLine());
+                    switch (x)
+                    {
+                        case 0:
+                            break;
+                        case 1:
+                            Console.WriteLine("Wybrano szyfrowanie!");
+                            Console.WriteLine("Podaj tekst jawny, ktory chcesz zaszyfrowac: ");
+                            plainText = Console.ReadLine();
+                            Console.WriteLine("Podaj slowo klucz: ");
+                            keyWord = Console.ReadLine();
+                            Encryption.Encrypt(keyWord, plainText);
+
+                            break;
+                        case 2:
+                            Console.WriteLine("Wybrano deszyfrowanie!");
+                            Console.WriteLine("Podaj tekst, ktory chcesz odszyfrowac: ");
+                            plainText = Console.ReadLine();
+                            Console.WriteLine("Podaj slowo klucz: ");
+                            keyWord = Console.ReadLine();
+
+                            break;
+                        default:
+                            Console.WriteLine("Podano nieprawidłowy numer!");
+                            continue;
+                    }   
+                } while (x != 0);    
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Znak nie jest typu INT");
+            }
+             Console.ReadKey();
+        }
+    }
+}
